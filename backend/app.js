dda0195006d24303606e487d1813c86502303214
aleksandraVkosmose <alexandera.mir0nowa@yandex.ru>
2023-06-08 01:57:42 +0400

    v27

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb1', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
