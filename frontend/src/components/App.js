5dfe9d832c19301306ef9867eb4d4bd22deb4dd7
aleksandraVkosmose <alexandera.mir0nowa@yandex.ru>
2023-06-08 22:46:44 +0400

    v32

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -49,7 +49,7 @@ function App() {
       })))
     })
       .catch(err => console.log(err))
-  }}, [isLoggedIn]);
+  }}, [isLoggedIn, navigate]);
 
   function closeAllPopups() {
     setIsEditProfilePopupOpen(false)
